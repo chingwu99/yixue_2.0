7531e5e0afe6126ffac14028da5c4629966ac37a
chingwu99 <henry88028802@hotmail.com>
2024-01-07 15:12:53 +0800

    feat: install prettier

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
 
 export default function Home() {
-  return <main className=" text-red-400">hhh</main>;
+  return <main className=" flex flex-wrap text-red-400">hhh</main>;
 }
