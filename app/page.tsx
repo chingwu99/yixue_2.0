db05fda7219a9440afa448f6ee8fba4e62d24371
chingwu99 <henry88028802@hotmail.com>
2024-01-07 16:17:20 +0800

    feat: add dark mode

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
 import Image from "next/image";
+import ModeToggle from "@/components/mode-toggle";
 
 export default function Home() {
-  return <main className=" flex flex-wrap text-red-400">hhh</main>;
+  return (
+    <main className=" flex flex-wrap text-red-400">
+      hhh <ModeToggle />
+    </main>
+  );
 }
